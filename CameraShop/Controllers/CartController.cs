420c98e8b04aa9b2848e45aede8a62c63d21a50e
Duc Le <77117549+duclee257@users.noreply.github.com>
2022-01-15 22:19:02 +0700

    add shopping cart

---
+++ CameraShop/Controllers/CartController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CameraShop.Models;
+
+namespace CameraShop.Controllers
+{
+    public class CartController : Controller
+    {
+        public DBContext db = new DBContext();
+        // GET: GioHang
+        public Cart getCart() {
+            Cart gioHang = Session["Cart"] as Cart;
+            if (gioHang == null || Session["Cart"] == null)
+            {
+                gioHang = new Cart();
+                Session["Cart"] = gioHang;
+            }
+            return gioHang;
+        }
+
+        //add item vao gio hang
+        public ActionResult AddtoCart(int? id)
+        {
+            var product = db.SanPham.SingleOrDefault(s => s.MaSP == id);
+            if (product != null)
+            {
+                getCart().Add(product); 
+            }
+            return RedirectToAction("ShowCart", "Cart");
+        }
+
+        //trang gio hang
+        public ActionResult ShowCart()
+        {
+            if (Session["Cart"] == null)
+                return RedirectToAction("ShowCart", "Cart");
+            Cart gioHang = Session["Cart"] as Cart;
+            return View(gioHang);
+
+        }
+        public ActionResult delete_SP(int id)
+        {
+            Cart gioHang = Session["Cart"] as Cart;
+            gioHang.delete_SP_Shopping(id);
+            return RedirectToAction("ShowCart", "Cart");
+        }
+
+
+    }
+}
---
+++ CameraShop/Models/Cart.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CameraShop.Models
+{
+    public class ItemCart
+    {
+        public int Id { get; set; }
+        public SanPham sanPham_shopping { get; set; }
+        public int soLuong_shopping { get; set; }
+
+    }
+    public class Cart
+
+    {
+        List<ItemCart> itemGioHangs = new List<ItemCart>();
+        public IEnumerable<ItemCart> ItemGioHangs
+        {
+            get { return itemGioHangs; }
+        }
+        public void Add(SanPham sanPham, int soLuong = 1)
+        {
+            var item = itemGioHangs.FirstOrDefault(s => s.sanPham_shopping.MaSP == sanPham.MaSP);
+            if (item == null)
+            {
+                itemGioHangs.Add(new ItemCart
+                {
+                    sanPham_shopping = sanPham,
+                    soLuong_shopping = soLuong
+                });
+
+            }
+            else
+            {
+                item.soLuong_shopping += soLuong;
+            }
+        }
+        public void Update_SL_Shopping(int ? id, int soLuong)
+        {
+            var item = itemGioHangs.Find(s => s.sanPham_shopping.MaSP == id);
+            if (item != null)
+            {
+                item.soLuong_shopping = soLuong;
+            }
+        }
+
+        public void delete_SP_Shopping(int id)
+        {
+            var item = itemGioHangs.Find(s => s.Id == id);
+            if (item != null)
+            {
+                itemGioHangs.Remove(item);
+            }
+        }
+    }
+}
